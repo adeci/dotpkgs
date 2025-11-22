206ba8e520cfa128a4b388f89765c3f1e3e47839
adeci <alex.decious@gmail.com>
2025-11-22 11:30:59 +0700

    swayconf

--- wrappers/sway/module.nix
+++ wrappers/sway/module.nix
@@ -4,9 +4,14 @@ let
     url = "https://raw.githubusercontent.com/adeci/wallpapers/main/tokyo-night/tokyo-night_nix.png";
     sha256 = "sha256-W5GaKCOiV2S3NuORGrRaoOE2x9X6gUS+wYf7cQkw9CY=";
   };
+  openTerminalCwdScript = pkgs.writeShellScript "open-terminal-cwd" ''
+    PATH=${pkgs.swaycwd}/bin:$PATH
+    ${builtins.readFile ./scripts/open-terminal-cwd.sh}
+  '';
   swayConfig = pkgs.runCommand "sway-config" { } ''
     substitute ${./config} $out \
       --replace "@backgroundImage@" "${backgroundImage}" \
+      --replace "@openTerminalCwdScript@" "${openTerminalCwdScript}"
   '';
 in
 {
