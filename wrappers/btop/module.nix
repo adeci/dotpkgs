d49e117d901c7af2c58b9dfdf2fda0c6d999a2cc
adeci <alex.decious@gmail.com>
2025-11-23 18:28:21 +0700

    prelude

--- wrappers/btop/module.nix
+++ wrappers/btop/module.nix
@@ -6,5 +6,9 @@
 
       "btop.conf".path = ./btop.conf;
 
+      # Add ROCm SMI library path for AMD GPU monitoring
+      extraPackages = [ pkgs.rocmPackages.rocm-smi ];
+      env.LD_LIBRARY_PATH = "${pkgs.rocmPackages.rocm-smi}/lib";
+
     }).wrapper;
 }
